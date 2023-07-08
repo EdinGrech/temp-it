183ff4d102163b950c02a29774dbfb469ba11ef7
EdinGrech <edin.grech@gmail.com>
2023-07-08 14:34:39 +0200

    client implimentation

---
+++ client/temp-it/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'temp-it',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
