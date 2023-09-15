7cade5b4340b272569e66cffd5237ad997086c8c
Edin Grech <edin.grech@gmail.com>
2023-09-16 00:43:42 +0200

    Feat: component styling

--- client/temp-it/src/app/components/sensor/collective-sensor-summery/collective-sensor-summery.component.ts
+++ client/temp-it/src/app/components/sensor/collective-sensor-summery/collective-sensor-summery.component.ts
@@ -1,14 +1,17 @@
 import { CommonModule } from '@angular/common';
 import {
   Component,
+  ElementRef,
   Input,
   OnChanges,
+  OnDestroy,
   OnInit,
   SimpleChanges,
+  ViewChild,
 } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { Store } from '@ngrx/store';
-import { Observable, map } from 'rxjs';
+import { BehaviorSubject, Observable, map, take, takeLast } from 'rxjs';
 import {
   ReducerSensorData,
   SensorDetails,
@@ -25,7 +28,7 @@ import { selectSensorAll } from 'src/app/state/sensor/sensor.selector';
   imports: [IonicModule, CommonModule],
   standalone: true,
 })
-export class CollectiveSensorSummeryComponent implements OnInit, OnChanges {
+export class CollectiveSensorSummeryComponent implements OnInit, OnChanges, OnDestroy {
   @Input() sensors?: SensorDetails[];
   sensorsData$?: Observable<ReducerSensorData[] | undefined>;
   averageTemperatureColor: string = 'primary';
@@ -34,71 +37,23 @@ export class CollectiveSensorSummeryComponent implements OnInit, OnChanges {
 
   onlineSensors: number = 0;
   offlineSensors: number = 0;
-
   averageTemperature: number = 0;
   averageHumidity: number = 0;
+  lastAlertRecords?: singleSensorData[][];
 
-  lastAlertRecords?: singleSensorData[];
+  @ViewChild('sensorList', { static: true }) sensorList?: ElementRef;
+  interval: any;
 
   constructor(private store: Store<AppState>) {
-    this.sensorsData$ = this.store.select(selectSensorAll).pipe(
-      map((res:ReducerSensorData[]) => {
-        if (!this.sensors) return res;
-        let temperatureSum = 0;
-        let humiditySum = 0;
-        let entrySum = 0;
-        res?.forEach((sensorData) => {
-          if (sensorData.sensorDataValues.length > 0) {
-            temperatureSum +=
-              sensorData.sensorDataValues[
-                sensorData.sensorDataValues.length - 1
-              ].temperature;
-            humiditySum +=
-              sensorData.sensorDataValues[
-                sensorData.sensorDataValues.length - 1
-              ].humidity;
-            entrySum++;
-          }
-          if (sensorData.alertFailIndexes.length > 0){
-            sensorData.alertFailIndexes.slice(-3).forEach((alertIndex) => {
-              let lastAlertRecord = sensorData.sensorDataValues[alertIndex];
-              if (lastAlertRecord) {
-                if (this.lastAlertRecords) {
-                  this.lastAlertRecords.push(lastAlertRecord);
-                } else {
-                  this.lastAlertRecords = [lastAlertRecord];
-                }
-              }
-            });
-          }
-        });
-        this.averageTemperature = temperatureSum / entrySum;
-        this.averageHumidity = humiditySum / entrySum;
-        // if (this.averageTemperature > 30) {
-        //   this.averageTemperatureColor = 'danger-high';
-        // } else if (this.averageTemperature < 10) {
-        //   this.averageTemperatureColor = 'danger-low';
-        // } else {
-        //   this.averageTemperatureColor = 'primary';
-        // }
-        // if (this.averageHumidity > 70) {
-        //   this.averageHumidityColor = 'danger-high';
-        // } else if (this.averageHumidity < 30) {
-        //   this.averageHumidityColor = 'danger-low';
-        // } else {
-        //   this.averageHumidityColor = 'primary';
-        // }
-        console.log(
-          "info: ",
-          this.averageTemperatureColor,
-          this.averageTemperature,
-          this.averageHumidity,
-          this.averageAlertColor,
-          this.lastAlertRecords
-        );
-        return res;
-      })
-    );
+    
+  }
+
+  scrollToTop() {
+    this.sensorList!.nativeElement.scrollTop = 0;
+  }
+  
+  scrollToBottom() {
+    this.sensorList!.nativeElement.scrollTop = this.sensorList!.nativeElement.scrollHeight;
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -116,15 +71,85 @@ export class CollectiveSensorSummeryComponent implements OnInit, OnChanges {
         }
       });
       this.store.dispatch(loadAllBasedSensorReadings({ sensorIds: sensorIds }));
+      this.store.select(selectSensorAll).pipe(
+        map((res:ReducerSensorData[]) => {
+
+          this.averageTemperature = 0;
+          this.averageHumidity = 0;
+  
+          this.lastAlertRecords = undefined;
+          let temperatureSum = 0;
+          let humiditySum = 0;
+          let entrySum = 0;
+          res?.forEach((sensorData) => {
+            if (sensorData.sensorDataValues.length > 0) {
+              temperatureSum +=
+                sensorData.sensorDataValues[
+                  sensorData.sensorDataValues.length - 1
+                ].temperature;
+              humiditySum +=
+                sensorData.sensorDataValues[
+                  sensorData.sensorDataValues.length - 1
+                ].humidity;
+              entrySum++;
+            }
+            if (sensorData.alertFailIndexes.length > 0){
+              let sensorLastAlertRecords: singleSensorData[] = [];
+              sensorData.alertFailIndexes.slice(-3).forEach((alertIndex) => {
+                let lastAlertRecord = sensorData.sensorDataValues[alertIndex];
+                if (lastAlertRecord) {
+                  sensorLastAlertRecords.push(lastAlertRecord);
+                }
+              });
+              if (sensorLastAlertRecords.length > 0){
+                if (this.lastAlertRecords) {
+                  this.lastAlertRecords.push(sensorLastAlertRecords);
+                } else {
+                  this.lastAlertRecords = [sensorLastAlertRecords];
+                }
+              }
+              console.log(this.lastAlertRecords);
+            }
+          });
+          this.averageTemperature =  Math.round(((temperatureSum / entrySum) + this.averageTemperature) / 2 * 100) / 100;
+          this.averageHumidity = Math.round(((humiditySum / entrySum) + this.averageTemperature) / 2 * 100) / 100;
+          // if (this.averageTemperature > 30) {
+          //   this.averageTemperatureColor = 'danger-high';
+          // } else if (this.averageTemperature < 10) {
+          //   this.averageTemperatureColor = 'danger-low';
+          // } else {
+          //   this.averageTemperatureColor = 'primary';
+          // }
+          // if (this.averageHumidity > 70) {
+          //   this.averageHumidityColor = 'danger-high';
+          // } else if (this.averageHumidity < 30) {
+          //   this.averageHumidityColor = 'danger-low';
+          // } else {
+          //   this.averageHumidityColor = 'primary';
+          // }
+          return res;
+        }),
+        take(this.sensors!.length + 1)
+      ).subscribe();
     }
   }
 
   ngOnInit() {
-    console.log('info: ', 
-    this.averageTemperatureColor,
-    this.averageTemperature,
-    this.averageHumidity,
-    this.averageAlertColor,
-    this.lastAlertRecords);    
+    this.interval = setInterval(() => {
+      this.toggleWiggle();
+    }, 20000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
+
+  toggleWiggle() {
+    if (!this.sensorList) return;
+    const element = this.sensorList.nativeElement;
+    setTimeout(() => {
+      element.classList.remove('wiggle');
+    }, 800);
+    element.classList.add('wiggle');
   }
 }
